3cb9c01180e7371e4afae303f19019e649a703b2
Sosed_11 <kost1991ya@mail.ru>
2022-10-12 22:12:55 +0500

    Первое дз готово

---
+++ src/Author.java
@@ -0,0 +1,44 @@
+import java.util.Objects;
+
+public class Author {
+    private final String name;
+    private final String surname;
+
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+
+    public String getName() {
+        return this.name;
+    }
+
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+
+    public String getFullName() {
+        return this.name + " " + this.surname;
+    }
+
+    //1.10. Методы объектов
+    public String toString() {
+        return this.name + " " + surname;
+    }
+
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+    }
+
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Author stephenKing = new Author("Stephen", "King");
+        Book theStand = new Book("The Stand", stephenKing, 1975);
+
+
+        Author levTolstoy = new Author("Lev", "Tolstoy");
+        Book warAndPeace = new Book("War and Peace", levTolstoy, 1865);
+
+
+        Author sergeyYesenin = new Author("Sergey", "Yesenin");
+        Book blackMan = new Book("Black man", sergeyYesenin, 1926);
+
+
+        System.out.println("Название книги: " + theStand.getTitle()
+                + " Автор: " + theStand.getAuthor().getName() + " " + theStand.getAuthor().getSurname()
+                + " Дата: " +theStand.getPublicationYear());
+
+
+        System.out.println("Название книги: " + warAndPeace.getTitle()
+                + " Автор: " + warAndPeace.getAuthor().getName() + " " + warAndPeace.getAuthor().getSurname()
+                + " Дата: " + warAndPeace.getPublicationYear());
+
+
+        System.out.println("Название книги: " + blackMan.getTitle()
+                + " Автор: " + blackMan.getAuthor().getName() + " " + blackMan.getAuthor().getSurname()
+                + " Дата: " + blackMan.getPublicationYear());
+
+
+        warAndPeace.setPublicationYear(1999);
 
     }
 }
